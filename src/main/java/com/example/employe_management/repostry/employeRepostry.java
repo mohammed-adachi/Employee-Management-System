20773e2e69b1d303c655660f981e7712dfbb8fd0
Mohamed <mohame@gmail.com>
2024-11-12 18:04:52 +0100

    Initial commit: Add EmployeeRepository interface

---
+++ src/main/java/com/example/employe_management/repostry/employeRepostry.java
@@ -0,0 +1,12 @@
+package com.example.employe_management.repostry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.employe_management.models.emolyer;
+
+public interface employeRepostry extends JpaRepository<emolyer, Integer> {
+public emolyer findByName(String name) ;
+public emolyer findByEmail(String email); 
+	// implementation goes here
+
+
+}
